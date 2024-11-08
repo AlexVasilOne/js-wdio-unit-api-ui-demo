84f542eb55df16ad24a34612e1a75b849f1cf260
aleksei_vasilev <aleksei.vasilev@mongodb.com>
2024-11-08 22:39:00 +0400

    Used foreach loop instead for of for better readability

--- src/specs/EPMESPROAD-6362.spec.js
+++ src/specs/EPMESPROAD-6362.spec.js
@@ -19,23 +19,23 @@ describe('Verify that "Select all metrics" checkbox set/unset all others check-b
     const checkboxesRagas = await $$('//h4[text()="Select RAGAS Metrics"]/ancestor::div[1]//input[@type="checkbox"]');
     const checkboxesDeepEval = await $$('//h4[text()="Select DeepEval Metrics"]/ancestor::div[1]//input[@type="checkbox"]');
     // Ragas checkboxes are set
-    for (const elem of checkboxesRagas) {
-      await expect(elem).toHaveAttribute('aria-checked', 'true');
-    }
+    checkboxesRagas.forEach(async (element) => {
+      await expect(element).toHaveAttribute('aria-checked', 'true');
+    });
     // DEEPEVAL checkboxes are unset
-    for (const elem of checkboxesDeepEval) {
-      await expect(elem).toHaveAttribute('aria-checked', 'false');
-    }
+    checkboxesDeepEval.forEach(async (element) => {
+      await expect(element).toHaveAttribute('aria-checked', 'false');
+    });
     // UNselecting all metrics checkboxes within RAGAS block
     checkboxSelectAllRagas.click();
     // Ragas checkboxes are unset
-    for (const elem of checkboxesRagas) {
-      await expect(elem).toHaveAttribute('aria-checked', 'false');
-    }
+    checkboxesRagas.forEach(async (element) => {
+      await expect(element).toHaveAttribute('aria-checked', 'false');
+    });
     // DEEPEVAL checkboxes are still unset
-    for (const elem of checkboxesDeepEval) {
-      await expect(elem).toHaveAttribute('aria-checked', 'false');
-    }
+    checkboxesDeepEval.forEach(async (element) => {
+      await expect(element).toHaveAttribute('aria-checked', 'false');
+    });
   });
 
   it('Verify while at least one checkbox is not set button "Start test" is not available', async () => {
