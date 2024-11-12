2772b099a4dc1410c378f1c2d2314954e92129a7
aleksei_vasilev <aleksei.vasilev@mongodb.com>
2024-11-12 22:21:43 +0400

    Changes done as per feedback

--- src/specs/EPMESPROAD-6375.spec.js
+++ src/specs/EPMESPROAD-6375.spec.js
@@ -6,13 +6,15 @@ describe('Verify that files uploading', () => {
     await $('//button[text()="+ Upload New Dataset"]').click();
   });
   it('Verify that the file can be deleted by clicking on the “x” button.', async () => {
-    let uploadLink = $('//section//div[text()="browse"]');
-    await expect(uploadLink).toBeClickable(); // uploading button is available
+    let uploadLink = await $('//section//div[text()="browse"]');
+    await expect(uploadLink).toBeEnabled(); // uploading button is available
     const filePath = path.join(process.cwd(), 'test-data/sample-file.csv');
     await $('input[type="file"]').addValue(filePath);
+    const fileCard = await $('//section//div[text()="test-data/sample-file.csv"]');
+    await expect(fileCard).toBeDisplayed; // file was added
     await $('div button[type="button"]').click(); // file delete button
     uploadLink = $('//section//div[text()="browse"]');
-    await expect(uploadLink).toBeClickable(); // uploading button is available again
+    await expect(uploadLink).toBeEnabled(); // uploading button is available again
   });
   it('Verify impossibility to upload not a .csv file', async () => {
     const filePath = path.join(process.cwd(), 'test-data/sample-file.txt');
