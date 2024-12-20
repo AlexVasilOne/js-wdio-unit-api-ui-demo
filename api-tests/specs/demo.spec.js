d4ecda90d3fe991ada091d987e3e7d70229cd43e
aleksei_vasilev <aleksei.vasilev@mongodb.com>
2024-12-20 16:55:04 +0400

    added helper to send request

--- api-tests/specs/demo.spec.js
+++ api-tests/specs/demo.spec.js
@@ -1,15 +1,10 @@
-const axios = require('axios');
+const {
+  sendRequest,
+} = require('../helpers/requests');
 
 describe('API Test Demo', () => {
   it('GET list of files', async () => {
-    const response = await axios({
-      method: 'get',
-      url: 'https://aist.lab.epam.com/api-assure/api/file/',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TOKEN_CURRENT_USER}`,
-      },
-    });
-    expect(response.status).to.equal(200); 
+    const response = await sendRequest('api/file/');
+    expect(response.status).to.equal(200);
   });
 });
